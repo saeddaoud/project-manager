ddb280729356edefbd5c17f95cad22f6245c0893
Saed Daoud <eng.saed.daoud@gmail.com>
2021-01-22 19:33:27 -0500

    created a map in sass to make typography responsive and consistent at breakpoints

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const Footer = () => {
-  return <footer className='footer'>footer</footer>;
+  return (
+    <footer className='footer flex flex-jcc'>Saed Daoud &copy;2021</footer>
+  );
 };
 
 export default Footer;
---
+++ frontend/src/redux/store.js
@@ -0,0 +1,17 @@
+import { applyMiddleware, createStore, combineReducers } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+
+const middleware = [thunk];
+
+const reducer = combineReducers({});
+
+const initialState = {};
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
