09fead14d5fe4edce06ae20f50022e3707ce1bd9
Saed Daoud <eng.saed.daoud@gmail.com>
2021-01-25 22:50:09 -0500

    added the content of the home and login page

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,8 +3,16 @@ import React from 'react';
 const HomeScreen = () => {
   return (
     <div className='page home-page'>
-      <div className='container flex flex-jcc'>
-        <div>Home</div>
+      <div className='container flex flex-jcc '>
+        <div className='card flex flex-jcsa flex-fdc'>
+          <h2>Welcome to</h2>
+          <h1>Project Manager</h1>
+          <p>
+            A web-based solution that allows you assign tasks to your employees,
+            while tracking the progress they make on the projects you are
+            working on!
+          </p>
+        </div>
       </div>
     </div>
   );
--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const LoginScreen = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  return (
+    <div className='page login-page'>
+      <div className='container flex flex-jcc '>
+        <form className='form flex flex-fdc flex-jcsa'>
+          <div className='input-control'>
+            <label>Email</label>
+            <input
+              type='email'
+              name='email'
+              value={email}
+              placeholder='user@example.com'
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className='input-control'>
+            <label>Password</label>
+            <input
+              type='password'
+              name='password'
+              value={password}
+              placeholder='Password'
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button type='submit' className='btn'>
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default LoginScreen;
