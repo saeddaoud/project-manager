c43f9e401aaca6225c0ea61495bbdf385f077f23
Saed Daoud <eng.saed.daoud@gmail.com>
2021-02-07 22:08:38 -0500

    made the nav menu responsive

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { logout } from '../redux/actions/employeeActions';
 
 const Header = () => {
+  const [show, setShow] = useState(false);
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.meFetch);
-  console.log(user);
   return (
     <div className='header'>
-      <nav className='nav'>
-        <div className='container flex'>
+      <div className='container '>
+        <nav className='nav flex'>
           <div className='nav__logo'>
             <h4>Project M</h4>
           </div>
@@ -36,8 +36,56 @@ const Header = () => {
               </li>
             </ul>
           </div>
-        </div>
-      </nav>
+          <div className='side-nav'>
+            <div
+              className={
+                !show
+                  ? 'humberger flex flex-fdc'
+                  : 'humberger toggle flex flex-fdc'
+              }
+              onClick={() => setShow(!show)}
+            >
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <div className={show ? 'side-nav__menu show' : 'side-nav__menu'}>
+              <ul className='menu__list flex flex-fdc my-2'>
+                {user ? (
+                  <li>
+                    <Link to='/profile/me' onClick={() => setShow(false)}>
+                      Dashboard
+                    </Link>
+                  </li>
+                ) : (
+                  <li>
+                    <Link to='/' onClick={() => setShow(false)}>
+                      Home
+                    </Link>
+                  </li>
+                )}
+                <li>
+                  {user ? (
+                    <Link
+                      to='/login'
+                      onClick={() => {
+                        dispatch(logout());
+                        setShow(false);
+                      }}
+                    >
+                      Logout
+                    </Link>
+                  ) : (
+                    <Link to='/login' onClick={() => setShow(false)}>
+                      Login
+                    </Link>
+                  )}
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
     </div>
   );
 };
