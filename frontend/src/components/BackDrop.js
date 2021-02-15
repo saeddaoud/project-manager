8c0b0fbc50f48a733f8f30b69e33322d49865038
Saed Daoud <eng.saed.daoud@gmail.com>
2021-02-15 02:01:27 -0500

    added a form to add projects from the frontend

---
+++ frontend/src/components/BackDrop.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const BackDrop = () => {
+  return <div className='back-drop'></div>;
+};
+
+export default BackDrop;
--- frontend/src/screens/ProjectsScreen.js
+++ frontend/src/screens/ProjectsScreen.js
@@ -6,13 +6,14 @@ import Projects from '../components/Projects';
 import Spinner from '../components/Spinner';
 import Message from '../components/Message';
 import { useHistory } from 'react-router-dom';
+import BackDrop from '../components/BackDrop';
 
 const ProjectsScreen = () => {
   const history = useHistory();
 
   const dispatch = useDispatch();
-  const { user } = useSelector((state) => state.meFetch);
-
+  const { user, loading: userLoading } = useSelector((state) => state.meFetch);
+  console.log(user);
   const { projects, error, loading, success } = useSelector(
     (state) => state.projectsFetch
   );
@@ -20,11 +21,14 @@ const ProjectsScreen = () => {
   const [keyword1, setKeyword1] = useState('');
   const [keyword, setKeyword] = useState('');
   const [status, setStatus] = useState('active');
+  const [showAddProjectForm, setShowAddProjectForm] = useState(false);
+  const [projectName, setProjectName] = useState('');
+  const [projectDescription, setProjectDescription] = useState('');
+  const [projectNameError, setProjectNameError] = useState('');
+  const [projectDescriptionError, setProjectDescriptionError] = useState('');
 
   useEffect(() => {
-    if (!user) {
-      history.push('/login');
-    } else if (keyword !== '') {
+    if (keyword !== '') {
       dispatch(fetchProjects({ keyword }));
       setKeyword('');
     } else if (status === 'all') {
@@ -40,9 +44,79 @@ const ProjectsScreen = () => {
     setKeyword1('');
   };
 
+  const addProjectHandler = (e) => {
+    e.preventDefault();
+    console.log(projectName, projectDescription);
+    if (projectName !== '' && projectDescription !== '') {
+      //add the project
+      setProjectNameError('');
+      setProjectDescriptionError('');
+      setProjectName('');
+      setProjectDescription('');
+      setShowAddProjectForm(false);
+    } else {
+      if (projectName === '') {
+        setProjectNameError("Project's name is required");
+      }
+      if (projectDescription === '') {
+        setProjectDescriptionError("Project's decsription is required");
+      }
+    }
+  };
+
   return (
     <div className='container'>
+      {showAddProjectForm && (
+        <>
+          <BackDrop />
+          <form className='form add-project-form' onSubmit={addProjectHandler}>
+            <div className='input-control'>
+              <label>Project's Name</label>
+              <input
+                type='text'
+                placeholder="Enter Project's Name"
+                value={projectName}
+                onChange={(e) => setProjectName(e.target.value)}
+              />
+              {projectNameError && <small>{projectNameError}</small>}
+            </div>
+            <div className='input-control' style={{ height: '140px' }}>
+              <label>Project's Description</label>
+              <textarea
+                placeholder="Enter Project's Name"
+                rows='5'
+                // cols='35'
+                value={projectDescription}
+                onChange={(e) => setProjectDescription(e.target.value)}
+              />
+              {projectDescriptionError && (
+                <small>{projectDescriptionError}</small>
+              )}
+            </div>
+            <div className='flex'>
+              <button className='btn btn--add' type='submit'>
+                Add Project
+              </button>
+              <button
+                className='btn'
+                onClick={() => setShowAddProjectForm(false)}
+              >
+                Cancel
+              </button>
+            </div>
+          </form>
+        </>
+      )}
+
       <div className='actions flex flex-aife flex-fdc my-1'>
+        <div className='add-container'>
+          <div
+            className='add-container__btn'
+            onClick={() => setShowAddProjectForm(true)}
+          >
+            <i className='far fa-plus-square'></i>
+          </div>
+        </div>
         <div className='flex flex-aife' style={{ width: '100%' }}>
           <div className='search'>
             <form
